76416f555585f6f79253b91ae3ecd4aa30e8d1dd
code-navigator <markbdean1@gmail.com>
2023-05-07 13:52:48 -0700

    Add default layout with header.

---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<template lang="pug">
+q-layout.shadow-2.rounded-borders(
+    view="lHh lpr lFf" 
+    container 
+    style="height: 100vh;" 
+)
+    q-header
+        q-toolbar
+            q-toolbar-title Cygnus OPS
+    slot
+
+</template>
