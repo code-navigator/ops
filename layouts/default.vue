0e4b3ab0a91b76e189d7b9d7d8c3b0de27ee0e48
code-navigator <markbdean1@gmail.com>
2023-05-07 14:03:04 -0700

    Add comments to layout.

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,19 @@
 <template lang="pug">
+
+//- Default layout
 q-layout.shadow-2.rounded-borders(
     view="lHh lpr lFf" 
     container 
     style="height: 100vh;" 
-)
+)   
+    //- Header
     q-header
         q-toolbar
             q-toolbar-title Cygnus OPS
-    slot
+
+    //- Insert page here
+    q-page-container
+        q-page.q-pa-md
+            slot
 
 </template>
